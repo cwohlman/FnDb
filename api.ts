eee3b5d2c071fb2b819567688472fd5406666df3
Joshua Ohlman <ohlman@hey.com>
2022-12-07 21:58:51 -0500

    More messiness

--- api.ts
+++ api.ts
@@ -1,3 +1,55 @@
+export class Bucket<Data> implements
+  Sink<Data, Entry<Data>>,
+  Iterable<Entry<Data>>,
+  Indexed<number, Entry<Data>>
+{
+  insert(value: Data): Entry<Data> | Error {
+    throw new Error("Not implemented");
+  }
+  cursor(): Cursor<Entry<Data>> {
+    throw new Error("Not implemented");
+  }
+  fetch(key: number): Entry<Data> {
+    throw new Error("Not implemented");
+  }
+  keys(): Iterable<number> {
+    throw new Error("Not implemented");
+  }
+}
+
+export class Query<TSource, TResult, TArgs extends any[]> {
+  constructor(
+    public readonly source: Iterable<TSource>,
+    public readonly iterator: (item: TSource, memo: TResult, ...args: TArgs) => TResult,
+    public readonly memo: TResult,
+    public readonly args: TArgs,
+  ) { }
+}
+
+interface Sink<Input, Output> {
+  insert(value: Input): Output | Error
+}
+
+interface Entry<T> {
+  id: number,
+  value: T,
+}
+
+interface Iterable<Data> {
+  cursor(): Cursor<Data>
+}
+
+interface Indexed<TKey extends string | number | boolean, Data> {
+  fetch(key: TKey): Data
+  keys(): Iterable<TKey>
+}
+
+interface Cursor<T> {
+  next(): T
+}
+
+
+
 export function reduce<Element, Args extends any[], Result>(
   source: Set<Element> | Db<Element>,
   fn: (item: Element, ...args: Args) => Result,
